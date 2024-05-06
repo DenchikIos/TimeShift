e6c75023ecae1aab3ce5ff41bff106c2b7c7aa70
Denis IOS Developer <denis.poddubnyy.111@gmail.com>
2024-05-06 21:15:23 +0300

    Initial Commit

---
+++ TimeShift/TimeShiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeShiftApp.swift
+//  TimeShift
+//
+//  Created by User on 06.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct TimeShiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
